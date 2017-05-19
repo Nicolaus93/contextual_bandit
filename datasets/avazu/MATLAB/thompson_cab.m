3ac4da0f0a00653365f7f94104661008aa5d7852
nicoloc <nico.campolongo@gmail.com>
2017-05-19 13:13:02 +0200

    changed X and Y format

--- datasets/avazu/MATLAB/thompson_cab.m
+++ datasets/avazu/MATLAB/thompson_cab.m
@@ -1,4 +1,4 @@
-function model = thompson_cab(X, Y, users, gamma, p, K)
+function model = thompson_cab(X, Y, users, gamma, p, minUsed)
 % Context aware clustering of bandits using Thompson Sampling
 % Inputs:
 %       - X
@@ -11,22 +11,22 @@ function model = thompson_cab(X, Y, users, gamma, p, K)
 %       - model
 tic
 
-T = size(X, 1)/K;           % number of training samples
-d = size(X, 2);             % dimension of the item vectors
-numUsers = max(users)+1;    % number or users (+1 since 0 is included)
-used = zeros(1, numUsers);  % number of times each user gets served
-minUsed = 1;
+T = size(X, 1);                     % number of training samples
+d = size(X, 3);                     % dimension of the item vectors
+K = size(X, 2);                     % num of item vectors per round
+numUsers = max(users)+1;            % number or users (+1 since 0 is included)
+used = zeros(1, numUsers);          % number of times each user gets served
 
 % Thompson sampling parameters
 R = 0.01;
 epsilon = 1/log(T);
-delta = 0.1;
+delta = 0.2;
 v = R * sqrt(24 / epsilon * d * log(1 / delta));
 
 % users parameters
-UM_inv = zeros(d, d, numUsers); % user inverse
-f = zeros(d, numUsers);         % f
-w_hat = f;                      % w^
+UM_inv = zeros(d, d, numUsers);     % user inverse
+f = zeros(d, numUsers);             % f
+w_hat = f;                          % w^
 
 for i = 1 : numUsers
     UM_inv(:,:,i) = eye(d);
@@ -39,20 +39,19 @@ model.tregret = 0;                      % total regret
 model.neighborhoodsize = zeros(1, T);   % tracks (selected) neighborhood size
 model.updatedsize = zeros(1, T);        % tracks no. of updated weigth vectors w
 
-for round = 1:T 
+for t = 1:T 
  
-    if 0 == mod(round - 1, 100)
-        fprintf('%d \n', round-1);
+    if 0 == mod(t - 1, 100)
+        fprintf('%d \n', t-1);        
     end
     
-    t = K*(round-1)+1;
-    user = users(t)+1;                  % user at round t
-    full_context = X(t:t+K-1,:);        % items at round t
+    user = users(t);                    % user at round t
+    full_context = squeeze(X(t,:,:));   % items at round t
     used(user) = used(user) + 1;        % update counter 
-    
+        
     % initialize neighboroods parameters
-    N = zeros(K,numUsers);     % neigborhood sets   
-    CB = zeros(K,numUsers);    % "confidence bounds for every user
+    N = zeros(K,numUsers);              % neigborhood sets   
+    CB = zeros(K,numUsers);             % "confidence bounds" for every user
     
     % store mu tilde
     mu_tilde = zeros(d, numUsers);
@@ -80,47 +79,42 @@ for round = 1:T
            N(k,j) = 1;
        end 
     end
-    
+        
     % computes associated aggregate quantities for the model estimate and
-    % confidence bound    
-    
-    %for k = 1 : K
-    %    mu_tilde_sum = sum(mu_tilde(:, find(N(k,:))),2);       
-    %    mu_tilde_avg = mu_tilde_sum / sum(N(k,:));
-    %    payoff(k) = full_context(k,:) * mu_tilde_avg;    
-    %end
-    
-    % vectorized
+    % confidence bound            
     mu_tilde_sum = mu_tilde * N';
     mu_tilde_avg = bsxfun(@rdivide, mu_tilde_sum, sum(N,2)');
-    payoff = sum(full_context .* mu_tilde_avg', 2);
-    
+    payoff = sum(full_context .* mu_tilde_avg', 2);    
+    % best action and its index
     [~, indexMax] = max(payoff);
-    x = X(t+indexMax-1, :)';
+    x = squeeze(X(t,indexMax,:));     % take it as a column vector      
        
     % update parameters
-    f(:, user) = f(:, user) + Y(t+indexMax-1) * x;
-    UM_inv(:,:,user) = UM_inv(:,:,user) - UM_inv(:,:,user) * x * x' * UM_inv(:,:,user) ...
-       / (1 + x'* UM_inv(:,:,user) * x);
-    w_hat(:,user) = UM_inv(:,:,user) * f(:, user);
-    if CB(indexMax,user) > gamma/4 * log(t+1)
-        continue
+    updated = 0;
+    alpha = 0.12;
+    if CB(indexMax,user) > alpha * gamma/4 * log(t+1)
+        f(:, user) = f(:, user) + Y(t, indexMax) * x;
+        UM_inv(:,:,user) = UM_inv(:,:,user) - UM_inv(:,:,user) * (x * x') * UM_inv(:,:,user) ...
+            / (1 + x'* UM_inv(:,:,user) * x);
+        w_hat(:,user) = UM_inv(:,:,user) * f(:, user);
+        updated = 1;
     else
         to_update = find(N(indexMax,:));
-        for j = 1:sum(N(indexMax,:))
-            if CB(indexMax,to_update(j)) < gamma/4 * log(t+1)
-                f(:, j) = f(:, j) + Y(t+indexMax-1) * x;
-                UM_inv(:,:,j) = UM_inv(:,:,j) - UM_inv(:,:,j) * x * x' * UM_inv(:,:,j) ...
+        for j = 1:sum(N(indexMax,:))            
+            if CB(indexMax,to_update(j)) < alpha * gamma/4 * log(t+1)
+                f(:, j) = f(:, j) + Y(t, indexMax) * x;
+                UM_inv(:,:,j) = UM_inv(:,:,j) - UM_inv(:,:,j) * (x * x') * UM_inv(:,:,j) ...
                     / (1 + x'* UM_inv(:,:,j) * x);
                 w_hat(:,j) = UM_inv(:,:,j) * f(:, j);
+                updated = updated + 1;
             end
         end
-    end          
+    end            
     
     % update regret
-    model.regret(round) = 1 - Y(t+indexMax-1);
-    model.neighborhoodsize(round) = sum(N(indexMax,:));
-    %model.updatedsize(t) = updated;
+    model.regret(t) = 1 - Y(t, indexMax);
+    model.neighborhoodsize(t) = sum(N(indexMax,:));
+    model.updatedsize(t) = updated;
 end
 
 model.cregret = cumsum(model.regret);
