287ce0dea6651aa73e80c993a1f131a76704db07
Nicolaus93 <nico.campolongo@gmail.com>
2017-06-19 14:13:35 +0200

    trying to work on server

--- avazu.py
+++ avazu.py
@@ -19,6 +19,8 @@ import time
 import os
 import argparse
 
+aa = 1
+
 def get_data(dataset):
     file_path = os.getcwd()
     d = os.path.join(os.sep, file_path, 'datasets/avazu')
@@ -166,3 +168,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
