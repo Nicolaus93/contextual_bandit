ab155e8b6dcbfae08b7201eb8fd877186ff00709
nicoloc <nico.campolongo@gmail.com>
2017-06-23 11:28:17 +0200

    select users based on num of yes clicks

--- datasets/avazu/extract.py
+++ datasets/avazu/extract.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import argparse
 
-def def_user(row):   
+def def_user(row):
     if row['device_id'] == 'a99f214a':
         user = 'ip-' + row['device_ip'] + '-' + row['device_model']
     else:
@@ -11,19 +11,22 @@ def def_user(row):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Preprocess a dataset.')
-    parser.add_argument(dest='-i', metavar='intr', type=int, nargs=1,
-                        help='number of interactions per user')
+    parser.add_argument('-i', dest='intr', metavar='threshold', type=int, nargs=1,
+                        help='number of yes clicks')
 
     args = parser.parse_args()
     intr = args.intr[0]
-    
     df = pd.read_csv('train.csv')
+    # introduce user_id
     cols = ['device_id', 'device_ip', 'device_model']
     df = df.assign(user_id=pd.Series(df[cols].apply(def_user, axis=1)).values)
-    n = df['user_id'].value_counts()[df['user_id'].value_counts()>=intr].index
+    # select only yes clicks
+    yes = df[df['click']==1]
+    # select user whose number of yes clicks is >= than intr 
+    n = yes['user_id'].value_counts()[yes['user_id'].value_counts()>=intr].index
     res = df.loc[df['user_id'].isin(n)]
     print(res.shape)
     us = len(res['user_id'].unique())
     print(us)
     name = 'filtered' + str(intr) + '.csv'
-    res.to_csv(name)
+    res.to_csv(name, index=False)
