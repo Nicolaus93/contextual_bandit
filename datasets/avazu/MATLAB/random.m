a7cd139f3b28ca1705abacffe75cf77c7d28fe17
nicoloc <nico.campolongo@gmail.com>
2017-06-27 10:38:56 +0200

    fixed regret calculation

--- datasets/avazu/MATLAB/random.m
+++ datasets/avazu/MATLAB/random.m
@@ -3,7 +3,7 @@ function cregret = random(T, K, Y)
 %   
 regret = zeros(1, T);
 for t=1:T    
-    action = randi(K);
-    regret(t) = 1 - Y(t, action);
+    action = randi(K);     
+    regret(t) = max(Y(t, :)) - Y(t, action);
 end
 cregret = cumsum(regret);
