702f14b16aba22bfe2b8e51ded5241da0775d2de
nicoloc <nico.campolongo@gmail.com>
2017-06-28 17:14:49 +0200

    run experiments

---
+++ script.py
@@ -0,0 +1,7 @@
+import os 
+
+os.system("python3 avazu_cab.py filtered_10yes_50no &")
+os.system("python3 avazu_cab.py filtered_20yes_80no &")
+os.system("python3 avazu_cab.py filtered_30yes_150no &")
+
+
