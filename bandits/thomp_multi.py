94c9c3a92f644e846e31516b1c8a5961dd1479b6
nicoloc <nico.campolongo@gmail.com>
2017-07-19 15:08:07 +0200

    modified algorithms

--- bandits/thomp_multi.py
+++ bandits/thomp_multi.py
@@ -1,7 +1,5 @@
 import numpy as np
-import random
 from .utils import get_random_state, sherman_morrison
-from numpy.linalg import inv
 
 
 class ThompMulti(object):
@@ -13,39 +11,23 @@ class ThompMulti(object):
 
     """
 
-    def __init__(self, users, d=128, delta=0.5, R=0.01, 
-                    epsilon=0.5, random_state=None, v=0):
+    def __init__(self, users, d=128, random_state=None, v=0):
 
-        self.numUsers = users # number of Users
+        self.numUsers = users  # number of Users
         self.d = d
         self.random_state = get_random_state(random_state)
-
         # Thompson sampling parameters
-        self.v = R * np.sqrt(24 / epsilon
-                             * self.d
-                             * np.log(1 / delta))
         self.v = v
         self._init_model()
 
-
     def _init_model(self):
-        """
-        """
-        self.mu_hat = np.zeros(shape=(self.numUsers,self.d))
-        self.mu_tilde = np.zeros(shape=(self.numUsers,self.d))
-        self.f = np.zeros(shape=(self.numUsers,self.d))
-        ###
-        self.B = np.zeros(shape=(self.numUsers,self.d,self.d))
-        ###
-        self.B_inv = np.zeros(shape=(self.numUsers,self.d,self.d))
+        self.mu_hat = np.zeros(shape=(self.numUsers, self.d))
+        self.f = np.zeros(shape=(self.numUsers, self.d))
+        self.B_inv = np.zeros(shape=(self.numUsers, self.d, self.d))
         for i, mat in enumerate(self.B_inv):
-            ###
-            self.B[i] = np.eye(self.d)
-            ###
             self.B_inv[i] = np.eye(self.d)
 
-
-    @profile
+    # @profile
     def get_action(self, context_array, user):
         """Return the action to perform
 
@@ -62,33 +44,37 @@ class ThompMulti(object):
         action_id : int
             Id of the action that will be recommended to the user.
         """
-        self.mu_tilde[user] = self.random_state.multivariate_normal(self.mu_hat[user].flat, self.v**2 * self.B_inv[user])
-        payoff = self.mu_tilde[user].dot(context_array.T)
+        # self.mu_tilde = self.random_state.multivariate_normal(
+        #     self.mu_hat[user].flat, self.v**2 * self.B_inv[user])
+        self.mu_tilde = np.random.multivariate_normal(
+            self.mu_hat[user].flat, self.v**2 * self.B_inv[user])
+        payoff = self.mu_tilde.dot(context_array.T)
         action_id = np.argmax(payoff)
         return action_id
 
-        # best_actions = np.argwhere(payoff == np.amax(payoff))
-        # if len(best_actions) > 1:
-        #     b = list(np.squeeze(best_actions))
-        #     best = random.sample(b,1)[0]
-        # else:
-        #     best = np.squeeze(best_actions)
-        # self.aaa.append(best)
-        # return best
-
-
-    @profile
+    # @profile
     def reward(self, x, reward, action_id, user):
         """
+        Update the model after receiving reward.
+
+        Parameters
+        ----------
+        user : int
+            id of the user
+
+        x : np.array
+            context of the action performed
+
+        reward : float or int
+            reward for the action taken
+
+        action_id : int
+            id of the action in the current context
+
+        user : int
+            id of the user
         """
         self.f[user] += reward * x
-        
-        # B_inv = sherman_morrison(self.B_inv[user], x)
-        # x = np.reshape(x, (-1, 1))
-        # self.B[user] += x.dot(x.T)
-        self.B[user] += np.outer(x,x)
-        B_inv = inv(self.B[user])
-
+        B_inv = sherman_morrison(self.B_inv[user], x)
         self.B_inv[user] = B_inv
         self.mu_hat[user] = B_inv.dot(self.f[user])
-
--- bandits/thomp_one.py
+++ bandits/thomp_one.py
@@ -1,36 +1,45 @@
+""" Thompson Sampling with Linear Payoff
+This module contains a class that implements Thompson Sampling with Linear
+Payoff. Thompson Sampling with linear payoff is a contexutal multi-armed bandit
+algorithm which assume the underlying relationship between rewards and contexts
+is linear. The sampling method is used to balance the exploration and
+exploitation. Please check the reference for more details.
+"""
 import numpy as np
 from .utils import get_random_state, sherman_morrison
-from numpy.linalg import inv
 
 
 class ThompsonOne(object):
     r"""
+    d : int
+        dimension of the context arrays
 
-    Parameters: todo
+    v : float
+        parameter controlling the variance
 
-    References: todo
+    random_state: {int, np.random.RandomState} (default: None)
+        If int, np.random.RandomState will used it as seed. If None, a random
+        seed will be used.
 
+    References
+    ----------
+    .. [1]  Shipra Agrawal, and Navin Goyal. "Thompson Sampling for Contextual
+            Bandits with Linear Payoffs." Advances in Neural Information
+            Processing Systems 24. 2011.
     """
 
-    def __init__(self, d=128, delta=0.5, R=0.01,
-                 epsilon=0.5, random_state=None):
-
+    def __init__(self, d=128, random_state=None, v=0):
         self.d = d
         self.random_state = get_random_state(random_state)
-        # Thompson sampling parameters
-        # self.v = R * np.sqrt(24 / epsilon
-        #                      * self.d
-        #                      * np.log(1 / delta))
-        self.v = 0.01
+        self.v = v
         self._init_model()
 
     def _init_model(self):
-        """
-        """
         self.mu_hat = np.zeros(shape=(self.d))
         self.f = np.zeros(shape=(self.d))
         self.B_inv = np.eye(self.d)
 
+    # @profile
     def get_action(self, context_array):
         """Return the action to perform
 
@@ -39,24 +48,29 @@ class ThompsonOne(object):
         context_array : numpy array
             K x d array containing contexts for every action.
 
-        user : int
-            Id of the user at the current round.
-
         Returns
         -------
         action_id : int
             Id of the action that will be recommended to the user.
         """
-        mu_tilde = self.random_state.multivariate_normal(self.mu_hat.flat,
-                                                         self.v**2 * self.B_inv)
+        mu_tilde = self.random_state.multivariate_normal(
+            self.mu_hat.flat, self.v**2 * self.B_inv)
         payoff = mu_tilde.dot(context_array.T)
         action_id = np.argmax(payoff)
         return action_id
 
+    # @profile
     def reward(self, x, reward, action_id):
-        """
+        """Reward the previous action with reward.
+
+        Parameters
+        ----------
+        x : int
+            The history id of the action to reward.
+
+        reward : dictionary
+            The dictionary {action_id, reward}, where reward is a float.
         """
         self.f += reward * x
-        x = x.reshape((-1, 1))
         self.B_inv = sherman_morrison(self.B_inv, x)
         self.mu_hat = self.B_inv.dot(self.f)
