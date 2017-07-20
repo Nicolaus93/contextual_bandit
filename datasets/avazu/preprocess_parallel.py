76929351fa86d42492d2262550d167df2be64805
Nicolaus93 <nico.campolongo@gmail.com>
2017-07-20 14:57:36 +0200

    paralleeel

--- datasets/avazu/preprocess_parallel.py
+++ datasets/avazu/preprocess_parallel.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from multiprocessing import cpu_count, Pool
 from preprocess_hashing import feature_hashing, one_normalize, conjunctions
+from functools import partial
 
 
 def parallelize(data, func):
@@ -12,12 +13,23 @@ def parallelize(data, func):
     pool.join()
     return data
 
+def par_conj(data, partitions, columns):
+   data_split = np.array_split(data, partitions)
+   pool = Pool(partitions)
+   func = partial(conjunctions, cols=columns)
+   data = pd.concat(pool.map(func, data_split))
+   pool.close()
+   pool.join()
+   return data
+
 
 cores = cpu_count()  # Number of CPU cores on your system
 partitions = cores  # Define as many partitions as you want
-df = pd.read_csv('most_frequent_10000.csv')  # Load data
+dataset = 'most_frequent_10000.csv'
+df = pd.read_csv(dataset)  # Load data
 co = [c for c in df.columns if c not in ['user_id', 'click']]
-df[co] = parallelize(df[co], conjunctions)
+df = par_conj(df, partitions, co)
+#df = parallelize(data, conjunctions)
 # df = parallelize(df, feature_hashing)
 # df = parallelize(df, one_normalize)
-df.to_csv('test.csv')
+df.to_csv('test.csv', index=False)
