222fde3c6737b1a8163012eda6608aa5f8f53eef
Nicolaus93 <nico.campolongo@gmail.com>
2017-07-20 20:29:35 +0200

    going paralleeeeell

--- datasets/avazu/preprocess_parallel.py
+++ datasets/avazu/preprocess_parallel.py
@@ -8,6 +8,52 @@ from sklearn.preprocessing import LabelEncoder
 from preprocess_hashing import feature_hashing, one_normalize, conjunctions, build_dataset
 
 
+def one_hot(X):
+    """
+    """
+    print("One-hot encoding")
+    cols = [i for i in X.columns.values if i not in ['user_id', 'click']]
+
+    def one_hot_encoding(x):
+        """
+        Perform one-hot encoding and normalize
+        """
+        x[np.where(x)[0]] = 1
+        return x
+
+    X_cat = X.reindex(columns=cols)
+    X_num = X.reindex(columns=[x for x in X.columns.values
+                               if x not in cols])
+    X_cat = X_cat.apply(one_hot_encoding, axis=1)
+    X = pd.merge(X_cat, X_num, left_index=True, right_index=True)
+    return X
+
+
+# def applyParallel(dfGrouped, func):
+#     with Pool(cpu_count()) as p:
+#         ret_list = p.map(func, [group for name, group in dfGrouped])
+#     return pd.concat(ret_list)
+
+#     # applyParallel(df.groupby(df.index), tmpFunc)
+
+
+# def build_parallel(dfGrouped, func):
+#     pool = Pool(partitions)
+#     data = pd.concat(pool.map(build_dataset, [group for name, group in dfGrouped]))
+#     pool.close()
+#     pool.join()
+#     return data
+
+
+def par_one_hot(data, partitions):
+    data_split = np.array_split(data, partitions)
+    pool = Pool(partitions)
+    data = pd.concat(pool.map(one_hot, data_split))
+    pool.close()
+    pool.join()
+    return data
+
+
 def parallelize(data, func, partitions):
     data_split = np.array_split(data, partitions)
     pool = Pool(partitions)
@@ -60,13 +106,15 @@ if __name__ == '__main__':
     # parallelize!
     cores = cpu_count()  # Number of CPU cores on your system
     partitions = cores  # Define as many partitions as you want
+    print('Reading dataset')
     df = pd.read_csv(dataset)  # Load data
     co = [c for c in df.columns if c not in ['user_id', 'click']]
-    # df = par_conjunctions(df, partitions, co)
+    df = par_conjunctions(df, partitions, co)
     df = par_feature_hashing(df, partitions, n_feat)
     print(df.head())
-    df = parallelize(df, one_normalize, partitions)
-    df.to_csv('test.csv', index=False)
+    # df = parallelize(df, one_hot_encoding, partitions)
+    df = par_one_hot(df, partitions)
+    print(df.head())
 
     # building dataset
     df = build_dataset(df, k)
