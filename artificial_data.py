c69e358dd422b118bf883fbbe53910ff5e087145
nicoloc <nico.campolongo@gmail.com>
2017-07-26 15:10:33 +0200

    added plots for neighborood and updated

--- artificial_data.py
+++ artificial_data.py
@@ -1,32 +1,51 @@
 import numpy as np
-from datasets.artificial_data.generate_data import artificial_data_generator
-from bandits import thomp_cab, thomp_multi, thomp_one, linucb_one
 import matplotlib.pyplot as plt
+import seaborn; seaborn.set()
 import time
+from datasets.artificial_data.generate_data import artificial_data_generator
+from bandits import thomp_cab, thomp_multi, thomp_one, linucb_one, linucb_multi
+from sklearn.preprocessing import normalize
+
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('{}  {:2.2f} sec'.format(method.__name__, (te - ts)))
+        return result
+    return timed
 
 
 def policy_generation(bandit, dim, t, numUsers):
 
-    if bandit == 'LinUcbOne0':
+    if bandit == 'ExploitOne':
         policy = linucb_one.LinUcbOne(d=dim, alpha=0)
 
-    if bandit == 'LinUcbOne1':
-        policy = linucb_one.LinUcbOne(d=dim, alpha=0.6)
+    if bandit == 'LinUcbOne':
+        policy = linucb_one.LinUcbOne(d=dim, alpha=0.2)
 
     if bandit == 'ThompCAB':
-        policy = thomp_cab.ThompCAB(numUsers, d=dim, gamma=0.05, delta=0.1,
-                                    R=0.02, epsilon=1 / np.log(t / numUsers),
-                                    random_state=4)
+        policy = thomp_cab.ThompCAB(numUsers, d=dim, gamma=0.1,
+                                    v=0.1)
 
     elif bandit == 'ThompMulti':
-        policy = thomp_multi.ThompMulti(numUsers, d=dim, delta=0.1, R=0.02,
-                                        epsilon=1 / np.log(t / numUsers),
-                                        random_state=4)
+        policy = thomp_multi.ThompMulti(
+            numUsers, d=dim, random_state=None, v=0.1)
+
+    elif bandit == 'ExploitMulti':
+        policy = thomp_multi.ThompMulti(
+            numUsers, d=dim, random_state=None, v=0)
 
     elif bandit == 'ThompsonOne':
-        policy = thomp_one.ThompsonOne(d=dim, delta=0.1, R=0.02,
-                                       epsilon=1 / np.log(t),
-                                       random_state=None)
+        policy = thomp_one.ThompsonOne(d=dim, random_state=None, v=0.1)
+
+    elif bandit == 'LinUcbMulti':
+        policy = linucb_multi.LinUcbMulti(numUsers, d=dim, alpha=0.8)
 
     elif bandit == 'random':
         policy = 0
@@ -34,99 +53,105 @@ def policy_generation(bandit, dim, t, numUsers):
     return policy
 
 
+@timeit
 def policy_evaluation(policy, bandit, X, Y, users):
-    print(bandit)
 
+    print(bandit)
     T, d, k = X.shape
     seq_error = [0] * T
 
-    # timing
-    start = time.time()
+    for t in range(T):
+        if bandit is not 'random':
+            if t % 10000 == 0:
+                print(t)
 
-    if bandit in ['ThompCAB', 'ThompMulti']:
-        for t in range(T):
-            # if t % 100 == 0:
-            #     print('round ' + str(t))
-
-            user = users[t]
-            full_context = X[t].T
-            action_id = policy.get_action(full_context, user)
-            reward = Y[t, action_id]
-
-            # update policy
-            policy.reward(full_context[action_id], reward, action_id, user)
-            seq_error[t] = seq_error[t - 1] + max(Y[t]) - reward
-
-    elif bandit in ['ThompsonOne', 'LinUcbOne1', 'LinUcbOne0']:
-        for t in range(T):
-            full_context = X[t].T
+            policy.set_user(users[t])
+            full_context = normalize(X[t].T)
             action_id = policy.get_action(full_context)
             reward = Y[t, action_id]
-
-            # update policy
             policy.reward(full_context[action_id], reward, action_id)
-            seq_error[t] = seq_error[t - 1] + max(Y[t]) - reward
 
-    elif bandit == 'random':
-        for t in range(T):
+        else:
             action_id = np.random.randint(0, k)
             reward = Y[t, action_id]
-            seq_error[t] = seq_error[t - 1] + max(Y[t]) - reward
 
-    end = time.time()
-    print('time: {} sec'.format(end - start))
+        if t == 0:
+            seq_error[t] = max(Y[t]) - reward
+        else:
+            seq_error[t] = seq_error[t - 1] + max(Y[t]) - reward
 
     return seq_error
 
 
+def regret_calculation(seq_error):
+    t = len(seq_error)
+    regret = [x / y for x, y in zip(seq_error, range(1, t + 1))]
+    return regret
+
+
 def main():
-    # model = 0.1 * np.array([[5,1,4,1,1,1,4,1,2,4],
-    #                         [5,1,1,4,2,1,5,1,5,1],
-    #                         [1,5,1,5,1,4,1,4,2,1],
-    #                         [1,5,4,2,5,1,1,5,1,1],
-    #                         [2,1,1,1,5,4,1,1,4,5]])
+    # model = np.array([[5, 1, 4, 1, 1, 1, 4, 1, 2, 4],
+    #                   [5, 1, 1, 4, 2, 1, 5, 1, 5, 1],
+    #                   [1, 5, 1, 5, 1, 4, 1, 4, 2, 1],
+    #                   [1, 5, 4, 2, 5, 1, 1, 5, 1, 1],
+    #                   [2, 1, 1, 1, 5, 4, 1, 1, 4, 5]])
 
-    # X, Y, users, model, users_feat = artificial_data_generator(T=10000, d=5, K=20, classes=4, numUsers=10, model=model)
-    X, Y, users, model, users_feat = artificial_data_generator(T=1000,
-                                                               d=30,
-                                                               K=20,
-                                                               classes=1,
-                                                               numUsers=1)
+    # model = 1 * (model - 2)
 
-    # print(model)
-    # print("users feat")
-    # print(users_feat)
-    # print(Y)
+    # X, Y, users, model, users_feat = artificial_data_generator(
+    #     T=10000, K=20, numUsers=40, model=model, random_state=42)
+
+    classes = 5
+    us = 200
+    X, Y, users, model, users_feat = artificial_data_generator(
+        T=10000, d=30, K=20, classes=classes, numUsers=us)
 
     T, d, k = X.shape
+    classes = len(model)
     numUsers = len(np.unique(users))
+    print('number of users: {}'.format(numUsers))
 
     cum_regret = {}
-    col = ['b', 'g', 'r', 'y', 'm']
+    regret = {}
+    col = ['b', 'g', 'r', 'y', 'm', 'k']
 
     # run algorithms
-    bandits = ['LinUcbOne0', 'LinUcbOne1', 'ThompsonOne', 'random']
+    bandits = ['ThompMulti', 'random', 'ThompCAB',
+               'ThompsonOne', 'LinUcbMulti', 'ExploitMulti']
     for i, bandit in enumerate(bandits):
         policy = policy_generation(bandit, d, T, numUsers)
         seq_error = policy_evaluation(policy, bandit, X, Y, users)
         cum_regret[bandit] = seq_error
-        time = len(cum_regret[bandit])
+        regret[bandit] = regret_calculation(seq_error)
+        t = len(cum_regret[bandit])
 
-        # if bandit == 'ThompCAB':
-        #     plt.plot(range(time), policy.neigh_size)
+        plt.figure(1, figsize=(12, 8))
+        plt.plot(range(t), cum_regret[bandit], c=col[i],
+                 ls='-', label=str(bandit), linewidth=1.5)
+        p1 = plt.legend(loc='upper left')
+        plt.xlabel('time')
+        plt.ylabel('cumulative regret')
 
-        plt.plot(range(time),
-                 cum_regret[bandit],
-                 c=col[i],
-                 ls='-',
-                 label=str(bandit))
+        plt.figure(2, figsize=(10, 7))
+        plt.plot(range(t), regret[bandit], c=col[i],
+                 ls='-', label=str(bandit), linewidth=1.3)
+        plt.ylim([0, 1.5])
+        p2 = plt.legend(loc='upper right')
         plt.xlabel('time')
         plt.ylabel('regret')
-        # axes = plt.gca()
-        plt.title("Regret Bound with respect to T")
+        plt.title("Regret plot with respect to T")
 
-    p = plt.legend(loc='upper left')
-    p.draggable(True)
+        if bandit == 'ThompCAB':
+            plt.figure(3)
+            plt.plot(range(t), policy.neigh_size)
+            plt.title('Neighborood size')
+
+            plt.figure(4)
+            plt.plot(range(t), policy.updated_size)
+            plt.title('Updated users with respect to T')
+
+    p1.draggable(True)
+    p2.draggable(True)
     plt.show()
 
 
--- datasets/artificial_data/generate_data.py
+++ datasets/artificial_data/generate_data.py
@@ -10,51 +10,53 @@ def get_random_state(random_state=None):
     return random_state
 
 
-def artificial_data_generator(T=1000, d=10, K=10, classes=5, numUsers=1, model=None):
+def artificial_data_generator(T=1000, d=10, K=10, classes=5, numUsers=1,
+                              model=None, random_state=None):
 
-    r = np.random.RandomState(seed=4)
+    r = get_random_state(random_state=random_state)
 
     # generate model
     if model is None:
         # model = np.random.randint(low=-1, high=2, size=(classes,d))
-        model = np.random.randint(2, size=(classes,d))
-        model[model==0] = -0.5
+        model = np.random.randint(2, size=(classes, d))
+        model[model == 0] = -1
     else:
         classes, d = model.shape
 
     # generate users_feat
-    users_feat = np.zeros(shape=(classes*numUsers,d))
+    users_feat = np.zeros(shape=(classes * numUsers, d))
     k = 0
     for i in range(classes):
         for j in range(numUsers):
             # add noise
             # a = np.random.rand(d) / 10
             # users_feat[k, :] = (model[i,:]+a) / np.sum(model[i,:]+a)
-            users_feat[k,:] = model[i,:]
+            users_feat[k, :] = model[i, :]
             k += 1
 
     # generate users
-    users = np.random.randint(classes*numUsers, size=T)
+    users = np.random.randint(classes * numUsers, size=T)
 
     # generate contexts
-    X = np.zeros(shape=(T,d,K))
+    X = np.zeros(shape=(T, d, K))
     for t in range(T):
-        Xt = r.randint(2, size=(d,K))
-        Xt[Xt==0] = -1
-        X[t,:,:] = normalize(Xt, axis=0)
+        Xt = r.randint(10, size=(d, K))
+        Xt[Xt == 0] = -1
+        X[t, :, :] = normalize(Xt, axis=0)
 
     # generate rewards
     # it could be done in the previous for loop
     # it's done here for clarity
-    Y = np.zeros(shape=(T,K))
+    Y = np.zeros(shape=(T, K))
     for t in range(T):
-        mean = users_feat[users[t]].dot(X[t,:,:])
+        mean = users_feat[users[t]].dot(X[t, :, :])
         for i in range(K):
-            Y[t,i] = r.normal(mean[i], 0.1)
+            Y[t, i] = r.normal(mean[i], 0.1)
 
     return X, Y, users, model, users_feat
 
 
 if __name__ == '__main__':
-    X, Y, users, model = artificial_data_generator(T=2, d=5, K=3, classes=2, numUsers=1, model=model)
-
+    X, Y, users, model, us_feat = artificial_data_generator(T=2, d=5, K=3,
+                                                            classes=2,
+                                                            numUsers=1)
