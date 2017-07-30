e8c44bdca7a38f6e266e4479f09f3361462cea64
Nicolaus93 <nico.campolongo@gmail.com>
2017-07-30 17:32:42 +0200

    checking duplicates between 0/1 in the same round

--- datasets/avazu/preprocess_parallel.py
+++ datasets/avazu/preprocess_parallel.py
@@ -35,6 +35,7 @@ def one_hot(X):
 def build_dataset(group, k):
     """
     Todo: check for duplicates between 1 and 0s
+    in the process..
     """
     print('building dataset...')
     lst = []  # list containing every round
@@ -42,7 +43,7 @@ def build_dataset(group, k):
     user_interactions = group.groupby('click')  # there will be 0/1
     try:
         ones = user_interactions.get_group(1)
-        ones = ones[~ones.duplicated(keep='first')]
+        # ones = ones[~ones.duplicated(keep='first')]
     except Exception as e:
         exceptions.append(e)
         # print('    not enough ones')
@@ -51,8 +52,13 @@ def build_dataset(group, k):
         zeros = user_interactions.get_group(0)
         zeros = zeros[~zeros.duplicated(keep='first')]  # discard duplicate
         for index_and_row in ones.iterrows():
-            r = pd.concat([zeros.sample(n=k - 1), index_and_row[1]
-                           .to_frame().transpose()])
+            # r = pd.concat([zeros.sample(n=k - 1), index_and_row[1]
+            #                .to_frame().transpose()])
+            good = False
+            while not good:
+                r = pd.concat([zeros.sample(n=k - 1), index_and_row[1]
+                              .to_frame().transpose()])
+                good = is_good_round(r, k)
             lst.append(r.iloc[np.random.permutation(len(r))])  # shuffle
     except Exception as e:
         exceptions.append(e)
@@ -61,12 +67,62 @@ def build_dataset(group, k):
     return lst
 
 
-def build_parallel(data, k):
+def is_good_round(rnd, k):
+    r = rnd[~rnd.drop('click', axis=1).duplicated(keep='first')]
+    n = r.shape[0]
+    if n < k:
+        return False, r
+    return True, r
+
+
+def build_nodupli(group, k):
+    """
+    Now it checks for duplicates between 1 and 0s
+    """
+    print('building dataset...')
+    lst = []  # list containing every round
+    # group by 0/1 clicks
+    # user_interactions = group.drop('hour', axis=1).groupby('click')
+    user_interactions = group.groupby('click')
+    try:
+        ones = user_interactions.get_group(1)
+    except Exception:
+        # this shouldn't happen but who knows :P
+        print('no ones')
+        return lst
+    if len(ones) > 5:
+        zeros = user_interactions.get_group(0)
+        zeros = zeros[~zeros.duplicated(keep='first')]  # discard duplicates
+        for i, index_and_row in enumerate(ones.iterrows()):
+            try:
+                r = pd.concat([index_and_row[1].to_frame().transpose(),
+                               zeros.sample(n=k - 1)])
+            except Exception:
+                # print('not enough zeros')
+                # there might be < 19 zeros after discarding duplicates
+                print(len(zeros))
+                return []
+            good, r = is_good_round(r, k)
+            count = 0
+            while not good:
+                count += 1
+                r = r.append(zeros.sample(n=1))
+                good, r = is_good_round(r, k)
+                if count > len(ones):
+                    break
+            if good:
+                lst.append(r.iloc[np.random.permutation(len(r))])  # shuffle
+    return lst
+
+
+def build_parallel(data, k, partitions):
     pool = Pool(partitions)
-    dfGrouped = df.groupby(['user_id'])
-    func = partial(build_dataset, k=k)
+    dfGrouped = data.groupby(['user_id'])
+    func = partial(build_nodupli, k=k)
     lst = pool.map(func, [group for name, group in dfGrouped])
+    print(len(lst))
     flat_list = [item for sublist in lst for item in sublist]
+    print(len(flat_list))
     # shuffle data
     random.shuffle(flat_list)
     data = pd.concat(flat_list)
@@ -117,11 +173,14 @@ if __name__ == '__main__':
     parser.add_argument('-conj', dest='conj', action='store_true',
                         default=False,
                         help='whether include feature conjunctions or not')
+    parser.add_argument(dest='name', metavar='name', type=str, nargs=1,
+                        help='name of dataset after preprocessing')
 
     args = parser.parse_args()
     dataset = args.dataset[0]
     k = args.k[0]
     n_feat = args.n_feat[0]
+    name = args.name[0]
     conj = args.conj
 
     # parallelize!
@@ -129,16 +188,39 @@ if __name__ == '__main__':
     partitions = cores  # Define as many partitions as you want
     print('Reading dataset')
     df = pd.read_csv(dataset)  # Load data
+
+    n = df.user_id.value_counts().index[:1000]
+    df = df[df['user_id'].isin(n)]
+    df.drop('hour', axis=1, inplace=True)
+
+    try:
+        to_drop = ['C1', 'C15', 'C16', 'C18', 'C19']
+        df.drop(to_drop, axis=1, inplace=True)
+    except Exception:
+        pass
+
+    # building dataset
+    df = build_parallel(df, k, partitions)
+    print(df.shape)
+
+    # need to reindex since we can have same row multiple times
+    df = df.reset_index(drop=True)
+
+    # conjunctions
     if conj:
         co = [c for c in df.columns if c not in ['user_id', 'click']]
         df = par_conjunctions(df, partitions, co)
+
+    # feature hashing
     df = par_feature_hashing(df, partitions, n_feat)
     print(df.head())
+    print(df.shape)
+
+    # one-hot encoding
     df = par_one_hot(df, partitions)
     print(df.head())
+    print(df.shape)
 
-    # building dataset
-    df = build_parallel(df, k)
     rewards = pd.DataFrame(df['click'])
     users = pd.DataFrame(df['user_id'])
     df = df.drop(['click', 'user_id'], 1)  # remove click and user_id
@@ -159,15 +241,16 @@ if __name__ == '__main__':
     # some info
     msg = 'The preprocessed dataset contains: \n  ' \
           '{} rounds \n  {} actions per round\n  ' \
-          '{} columns per action'.format(t // k, k, d)
+          '{} columns per action\n'.format(t // k, k, d)
     print(msg)
 
     # save everything in hdf5 files
     print('saving...')
     file_path = os.getcwd()
-    directory = os.path.join(file_path, dataset)
-    directory = os.path.splitext(directory)[0] + \
-        '_k' + str(k) + '_d' + str(n_feat)
+    directory = os.path.join(file_path, name)
+    # directory = os.path.splitext(directory)[0] + \
+    #     '_k' + str(k) + '_d' + str(n_feat)
+    directory = os.path.splitext(directory)[0] + '_d' + str(n_feat)
     if conj:
         directory += '_conj'
     if not os.path.exists(directory):
@@ -178,17 +261,6 @@ if __name__ == '__main__':
     data.create_dataset('y', data=rewards)
     data.create_dataset('users', data=users)
     data.close()
-
-
-    # X = h5py.File(os.path.join(directory, 'X.hdf5'), 'w')
-    # X.create_dataset('X', data=df, compression='gzip', compression_opts=5)
-    # X.close()
-    # Y = h5py.File(os.path.join(directory, 'Y.hdf5'), 'w')
-    # Y.create_dataset('Y', data=rewards)
-    # Y.close()
-    # U = h5py.File(os.path.join(directory, 'users.hdf5'), 'w')
-    # U.create_dataset('users', data=users)
-    # U.close()
 
     f = open(os.path.join(directory, 'info.txt'), 'w')
     f.write(usr_msg)
