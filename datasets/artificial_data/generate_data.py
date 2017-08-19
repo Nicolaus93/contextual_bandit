d4cae9ec3acaca7470153c8c38848e2c148f5caa
nicoloc <nico.campolongo@gmail.com>
2017-08-19 23:17:34 +0200

    added arguments from command line

--- datasets/artificial_data/generate_data.py
+++ datasets/artificial_data/generate_data.py
@@ -1,5 +1,8 @@
 import numpy as np
+import argparse
 from sklearn.preprocessing import normalize
+import h5py
+import os
 
 
 def get_random_state(random_state=None):
@@ -17,9 +20,10 @@ def artificial_data_generator(T=1000, d=10, K=10, classes=5, numUsers=1,
 
     # generate model
     if model is None:
-        # model = np.random.randint(low=-1, high=2, size=(classes,d))
-        model = np.random.randint(2, size=(classes, d))
-        model[model == 0] = -1
+        # model = np.random.randint(low=-1, high=2, size=(classes, d))
+        model = np.random.random((classes, d))
+        # model = np.random.randint(2, size=(classes, d))
+        # model[model == 0] = -1
     else:
         classes, d = model.shape
 
@@ -37,11 +41,29 @@ def artificial_data_generator(T=1000, d=10, K=10, classes=5, numUsers=1,
     # generate users
     users = np.random.randint(classes * numUsers, size=T)
 
+    # generate users
+    users = []
+    tot = classes * numUsers
+    for i in range(tot):
+        users += [i] * T
+
+    users = np.random.permutation(np.asarray(users))
+    T = T * tot
+
     # generate contexts
     X = np.zeros(shape=(T, d, K))
     for t in range(T):
-        Xt = r.randint(10, size=(d, K))
-        Xt[Xt == 0] = -1
+        # Xt = r.randint(10, size=(d, K))
+        # Xt[Xt == 0] = -1
+        # Xt = np.random.random((d, K))
+
+        # sample uniformly between -1 and 1
+        Xt = 2 * np.random.random_sample((d, K)) - 1
+
+        # sample uniformly between 0 and 1
+        # Xt = np.random.random_sample((d, K))
+
+        # normalize
         X[t, :, :] = normalize(Xt, axis=0)
 
     # generate rewards
@@ -57,6 +79,47 @@ def artificial_data_generator(T=1000, d=10, K=10, classes=5, numUsers=1,
 
 
 if __name__ == '__main__':
-    X, Y, users, model, us_feat = artificial_data_generator(T=2, d=5, K=3,
-                                                            classes=2,
-                                                            numUsers=1)
+    parser = argparse.ArgumentParser(
+        description='Generate synthetic data')
+    parser.add_argument('-save', dest='save', action='store_true',
+                        default=False,
+                        help='whether to save data')
+    parser.add_argument('-k', dest='k', metavar='items_per_round',
+                        type=int, nargs=1,
+                        help='number of items per round')
+    parser.add_argument('-d', dest='d', metavar='features',
+                        type=int, nargs=1,
+                        help='dimension of features')
+    parser.add_argument('-t', dest='t', metavar='interactions',
+                        type=int, nargs=1,
+                        help='interactions per user')
+    parser.add_argument('-c', dest='c', metavar='classes',
+                        type=int, nargs=1,
+                        help='number of classes')
+    parser.add_argument('-u', dest='u', metavar='users',
+                        type=int, nargs=1,
+                        help='number of users per class')
+
+    args = parser.parse_args()
+    save = args.save
+    k = args.k[0]
+    d = args.d[0]
+    t = args.t[0]
+    c = args.c[0]
+    u = args.u[0]
+
+    X, Y, users, model, us_feat = artificial_data_generator(T=t, d=d, K=k,
+                                                            classes=c,
+                                                            numUsers=u)
+    if save:
+        file_path = os.getcwd()
+        dataset = str(c) + "_classes" + str(u) + "_users" + str(t) + "_rounds"
+        directory = os.path.join(file_path, dataset)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        print("saving..")
+        data = h5py.File(os.path.join(directory, 'dataset.hdf5'), 'w')
+        data.create_dataset('X', data=X)
+        data.create_dataset('y', data=Y)
+        data.create_dataset('users', data=users)
+        data.close()
