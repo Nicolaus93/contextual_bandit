7e2a6046bee00ff55c58dd7d5fb3691509618fd1
Nicolaus93 <nico.campolongo@gmail.com>
2017-10-05 19:04:24 +0200

    Delete script.py

--- script.py
+++
@@ -1,7 +0,0 @@
-import os 
-
-os.system("python3 avazu_cab.py filtered_10yes_50no &")
-os.system("python3 avazu_cab.py filtered_20yes_80no &")
-os.system("python3 avazu_cab.py filtered_30yes_150no &")
-
-
